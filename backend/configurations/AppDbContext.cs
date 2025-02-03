b7126ed693a42783fec81839c09c6d25d3a2afcc
Ivanilson Ferreira <ivanilson.ferreira.mec@gmail.com>
2025-02-03 11:51:28 -0300

    feat: add AppDbContext for database configuration and migrations

---
+++ backend/configurations/AppDbContext.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using backend.models;
+using System;
+using System.IO;
+
+namespace backend.configurations
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public AppDbContext()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            _configuration = configuration;
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    throw new InvalidOperationException("Database connection string is missing.");
+                }
+
+                optionsBuilder.UseMySql(
+                    connectionString,
+                    new MySqlServerVersion(new Version(8, 0, 32))
+                );
+            }
+        }
+    }
+}
